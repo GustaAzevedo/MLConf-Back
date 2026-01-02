edb9058609332a8ce5659a24ba9b160e71522331
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2026-01-02 01:15:21 -0300

    chore: bootstrap spring boot project

---
+++ src/main/java/com/mlconf/MLConfApplication.java
@@ -0,0 +1,12 @@
+package com.mlconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLConfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MLConfApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mlconf/MLConfApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mlconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MLConfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
